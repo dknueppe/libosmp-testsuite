1590721481f3d746c0358ee6854ceaa874d33941
Daniel Knüppe <daniel_k@posteo.net>
2019-07-26 17:03:44 +0200

    fixed test macros

--- testcase_01.c
+++ testcase_01.c
@@ -9,30 +9,30 @@ int main(int argc, char* argv[])
     OSMP_Datatype dt = OSMP_BYTE;
     OSMP_Request req = NULL;
 
-    TEST(OSMP_Size(&size));
-    TEST(OSMP_Rank(&rank));
-    TEST(OSMP_Send(send_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, dest));
-    TEST(OSMP_Recv(recv_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, &src, &len));
-    TEST(OSMP_CreateRequest(req));
-    TEST(OSMP_Isend(send_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, dest, req));
-    TEST(OSMP_Irecv(recv_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, &src, &len, req));
-    TEST(OSMP_Test(req, &flag));
-    TEST(OSMP_Wait(req));
-    TEST(OSMP_RemoveRequest(req));
+    TEST_NEG(OSMP_Size(&size));
+    TEST_NEG(OSMP_Rank(&rank));
+    TEST_NEG(OSMP_Send(send_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, dest));
+    TEST_NEG(OSMP_Recv(recv_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, &src, &len));
+    TEST_NEG(OSMP_CreateRequest(req));
+    TEST_NEG(OSMP_Isend(send_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, dest, req));
+    TEST_NEG(OSMP_Irecv(recv_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, &src, &len, req));
+    TEST_NEG(OSMP_Test(req, &flag));
+    TEST_NEG(OSMP_Wait(req));
+    TEST_NEG(OSMP_RemoveRequest(req));
 
     OSMP_Init(&argc, &argv);
-    //OSMP_Finalize();
+    OSMP_Finalize();
 
-    TEST(OSMP_Size(&size));
-    TEST(OSMP_Rank(&rank));
-    TEST(OSMP_Send(send_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, dest));
-    TEST(OSMP_Recv(recv_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, &src, &len));
-    TEST(OSMP_CreateRequest(req));
-    TEST(OSMP_Isend(send_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, dest, req));
-    TEST(OSMP_Irecv(recv_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, &src, &len, req));
-    TEST(OSMP_Test(req, &flag));
-    TEST(OSMP_Wait(req));
-    TEST(OSMP_RemoveRequest(req));
+    TEST_NEG(OSMP_Size(&size));
+    TEST_NEG(OSMP_Rank(&rank));
+    TEST_NEG(OSMP_Send(send_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, dest));
+    TEST_NEG(OSMP_Recv(recv_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, &src, &len));
+    TEST_NEG(OSMP_CreateRequest(req));
+    TEST_NEG(OSMP_Isend(send_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, dest, req));
+    TEST_NEG(OSMP_Irecv(recv_buffer, OSMP_MAX_PAYLOAD_LENGTH, dt, &src, &len, req));
+    TEST_NEG(OSMP_Test(req, &flag));
+    TEST_NEG(OSMP_Wait(req));
+    TEST_NEG(OSMP_RemoveRequest(req));
 
     printf("%s finished successful!\n", argv[0]);
     return OSMP_SUCCESS;
--- testsuite.h
+++ testsuite.h
@@ -5,5 +5,13 @@
 #include "osmplib.h"
 
 #define TEST(func) \
-    if((func) != OSMP_ERROR) \
-        printf("Error in File %s at line %d\n ", __FILE__, __LINE__);
+    if((func) == OSMP_ERROR) \
+        printf("Error in File %s at line %d\n ", __FILE__, __LINE__); \
+        fflush(NULL); \
+        return(EXIT_FAILURE);
+
+#define TEST_NEG(func) \
+    if((func) != OSMP_ERROR) { \
+        printf("Error in File %s at line %d\n ", __FILE__, __LINE__); \
+        fflush(NULL); \
+        return(EXIT_FAILURE); }
